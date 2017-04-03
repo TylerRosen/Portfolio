254b7d1aef62aa8b54561723768ff564c70ed219
TylerRosen <tylermrosen@gmail.com>
2017-04-03 17:13:54 -0400

    Created buttons to change black/white thems

---
+++ assets/javascript/logic.js
@@ -0,0 +1,7 @@
+$('#black-theme').click(function() {
+    $('link').attr('href', 'assets/css/style-black.css');
+});
+
+$('#white-theme').click(function() {
+    $('link').attr('href', 'assets/css/style-white.css');
+});
